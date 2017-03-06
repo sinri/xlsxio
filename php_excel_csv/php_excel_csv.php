f91db0f94ca9fdbe6361313ba1262ad645ff1289
倪 李俊 <everstray@yeah.net>
2017-03-06 09:47:08 +0800

    php-written excel csv conventor

---
+++ php_excel_csv/php_excel_csv.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * POST TO THIS FILE
+ * RESPONSE CSV
+ */
+if ($_FILES["file"]["error"] > 0){
+    // echo "Error: " . $_FILES["file"]["error"] . "<br />";
+    response("FAIL","File Upload Error: ".$_FILES["file"]["error"]);
+}
+else{
+    // echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+    // echo "Type: " . $_FILES["file"]["type"] . "<br />";
+    // echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+    // echo "Stored in: " . $_FILES["file"]["tmp_name"];
+
+    //FILE SIZE > 10, exit
+    if($_FILES["file"]["size"]/1024.0/1024.0>10){
+        response("FAIL","File Upload Too Large: ".($_FILES["file"]["size"])." bytes");
+    }
+
+    $allowed = array(
+        "xls" => array( "application/vnd.ms-excel" ),
+        "xlsx" => array(
+            "application/vnd.ms-excel",
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
+    );
+    if(!in_array($_FILES['file']['type'], $allowed['xlsx'])){
+        response("FAIL","File Upload cannot be treated as EXCEL file: ".$_FILES['file']['type']);
+    }
+
+    $command="SinriExcel2CSV -i ".escapeshellarg($_FILES["file"]["tmp_name"]);
+    if(!empty($_REQUEST['sheet_name'])){
+        $command.=" ".escapeshellarg($_REQUEST['sheet_name']);
+    }
+    exec($command,$csv_lines,$return_var);
+    if(!empty($csv_lines)){
+        $csv_lines=implode("\n", $csv_lines);
+    }else{
+        $csv_lines="";
+    }
+    if($return_var){
+        response("FAIL","SinriExcel2CSV Error: ".PHP_EOL.$csv_lines);
+    }
+
+    response("OK",$csv_lines);
+}
+
+function response($result,$data){
+    echo json_encode(array("result"=>$result,"data"=>$data));
+    exit();
+}
